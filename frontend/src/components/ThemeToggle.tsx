22fa2b9bd0c9be4eeb2108a7ac414c94a6b67098
allysson-assuncao <anybody723@proton.me>
2024-10-07 11:17:47 -0300

    project running apparently fine, managed to split the mode and color change in theme, needs some work yet

--- frontend/src/components/ThemeToggle.tsx
+++ frontend/src/components/ThemeToggle.tsx
@@ -11,26 +11,63 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import {useEffect, useState} from "react";
 
 export function ThemeToggle() {
     const { setTheme, themes } = useTheme()
 
+    const modes = [
+        'light',
+        'dark',
+    ]
+
+    const colors = [
+        'green',
+        'purple',
+    ]
+
+    const [mode, setMode] = useState<string>('light');
+    const [color, setColor] = useState<string>('green');
+
+    useEffect(() => {
+        setTheme(mode + '-' + color)
+        console.log(mode + '-' + color)
+    }, [mode, color, setTheme, themes]);
+
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon">
-                    <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                    <span className="sr-only">Alternar tema</span>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-                {themes.map(theme => (
-                    <DropdownMenuItem key={theme} onClick={() => setTheme(theme)}>
-                        {(theme)}
-                    </DropdownMenuItem>
-                ))}
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <div>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="outline" size="icon">
+                        <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                        <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                        <span className="sr-only">Alternar tema</span>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    {modes.map(mode => (
+                        <DropdownMenuItem key={mode} onClick={() => setMode(mode)}>
+                            {(mode)}
+                        </DropdownMenuItem>
+                    ))}
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="outline" size="icon">
+                        <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                        <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                        <span className="sr-only">Alternar tema</span>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    {colors.map(color => (
+                        <DropdownMenuItem key={color} onClick={() => setColor(color)}>
+                            {(color)}
+                        </DropdownMenuItem>
+                    ))}
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div>
     )
 }
